cd4a8dcab767ac5354f169dc949e0a1c073f0449
Emzzy241 <emzzyoluwole@gmail.com>
2025-04-29 11:41:14 +0100

    fix blank page bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Rhemation2/',
   server: {
     port: 5173, // Ensure this port is available
     host: true, // Allow access from other devices on the network
