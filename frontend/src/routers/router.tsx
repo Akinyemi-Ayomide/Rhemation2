256478f92bcdaab88296e98464150b83ec1f0acd
Emzzy241 <emzzyoluwole@gmail.com>
2025-04-29 11:14:53 +0100

    fix router bug

--- frontend/src/routers/router.tsx
+++ frontend/src/routers/router.tsx
@@ -5,18 +5,50 @@ import ContactScreen from '../Screens/ContactScreen';
 import AboutScreen from '../Screens/AboutScreen';
 import HomeScreenDetails from '../Screens/HomeScreenDetails';
 import RateScreen from '../Screens/RateScreen';
-const router = createBrowserRouter([
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <HomeScreen /> },
+        { path: 'contact', element: <ContactScreen /> },
+        { path: 'about', element: <AboutScreen /> },
+        { path: 'rate', element: <RateScreen /> },
+        { path: 'details/:id', element: <HomeScreenDetails /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <HomeScreen /> },
-      { path: 'contact', element: <ContactScreen /> },
-      { path: 'about', element: <AboutScreen /> },
-      { path: 'rate', element: <RateScreen /> },
-      { path: 'details/:id', element: <HomeScreenDetails /> },
-    ],
-  },
-]);
+    basename: '/Rhemation2/',
+  }
+);
 
 export default router;
+
+
+
+
+// import { createBrowserRouter } from 'react-router-dom';
+// import RootLayout from '../Layout/RootLayout';
+// import HomeScreen from '../Screens/HomeScreen';
+// import ContactScreen from '../Screens/ContactScreen';
+// import AboutScreen from '../Screens/AboutScreen';
+// import HomeScreenDetails from '../Screens/HomeScreenDetails';
+// import RateScreen from '../Screens/RateScreen';
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <RootLayout />,
+//     children: [
+//       { index: true, element: <HomeScreen /> },
+//       { path: 'contact', element: <ContactScreen /> },
+//       { path: 'about', element: <AboutScreen /> },
+//       { path: 'rate', element: <RateScreen /> },
+//       { path: 'details/:id', element: <HomeScreenDetails /> }
+//     ],
+//   },
+// ]);
+
+// export default router;
